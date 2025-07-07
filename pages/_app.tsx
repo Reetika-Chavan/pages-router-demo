02995d84fdacac77f5d9e28983008206be976eb8
Reetika-Chavan <reetika.chavan@contentstack.com>
2025-07-07 12:14:00 +0530

    changes in contentsatck.ts

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+// pages/_app.tsx
+import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import ContentstackLivePreview from '@contentstack/live-preview-utils'
+
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      console.log('✅ Initializing Contentstack Live Preview')
+      ContentstackLivePreview.init({
+        enable: true,
+        clientUrlParams: {
+          host: process.env.NEXT_PUBLIC_CONTENTSTACK_APP_HOST!,
+        },
+      })
+    }
+  }, [])
+
+  return <Component {...pageProps} />
+}
